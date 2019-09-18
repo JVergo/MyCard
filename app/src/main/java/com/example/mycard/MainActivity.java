2bee7b8093a3e2e60d3acd28750330f5357afe0c
Vergo-Laptop <jvergoa@gmail.com>
2019-09-18 12:52:40 +0200

    1. st commit set up menu layout

--- app/src/main/java/com/example/mycard/MainActivity.java
+++ app/src/main/java/com/example/mycard/MainActivity.java
@@ -9,31 +9,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView editView, setView;
-    ProgressBar pb;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editView = findViewById(R.id.EditView);
-        setView = findViewById(R.id.SetView);
-        pb = findViewById(R.id.progressBar);
     }
 
-    public void changeText(View view) {
-        setView.setText(editView.getText());
-    }
-
-    public void changeProgressBar(View view) {
-        String change = view.getTag().toString();
-        if(change == "add") {
-            pb.setProgress(pb.getProgress() + 1);
-        }
-        else if (change == "sub")
-        {
-            pb.setProgress(pb.getProgress() - 1);
-        }
-    }
 }
